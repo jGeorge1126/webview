31bc281e345bb88c39b8b8042b57b9c750c020ba
jGeorge1126 <georgecupid1126@gmail.com>
2024-08-15 10:49:26 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# Webview
+
+An app built with Flutter that has a webview that renders a webapp url as an application.
+
+## Getting Started
+
+Make sure you have the latest flutter sdk and Android Studio installed.
+
+Fork and clone this repo in your Android Studio
+
+Open pubspec.yaml and click on 'Pub get' or open Android Studio terminal and run
+
+```bash
+flutter pub get
+```
+
+This will install all dependencies
+
+## Changing splash screen and no internet images
+
+Go to /assets/images and change them, if you choose an image with a different name to the one
+provided, then go to pubspec.yaml and main.dart and update the names their too.
---
+++ lib/main.dart
@@ -0,0 +1,200 @@
+import 'dart:async';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+import 'package:connectivity_plus/connectivity_plus.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  final Connectivity _connectivity = Connectivity();
+  late StreamSubscription<ConnectivityResult> _connectivitySubscription;
+  bool _isOnline = true; // Initial assumption
+
+  @override
+  void initState() {
+    super.initState();
+    _initConnectivity();
+    _connectivitySubscription = _connectivity.onConnectivityChanged.listen(_updateConnectionStatus);
+  }
+
+  @override
+  void dispose() {
+    _connectivitySubscription.cancel();
+    super.dispose();
+  }
+
+  Future<void> _initConnectivity() async {
+    ConnectivityResult result;
+    try {
+      result = await _connectivity.checkConnectivity();
+      _updateConnectionStatus(result);
+    } catch (e) {
+      if (kDebugMode) {
+        print("Couldn't check connectivity status: $e");
+      }
+      return;
+    }
+  }
+
+  void _updateConnectionStatus(ConnectivityResult result) {
+    setState(() {
+      _isOnline = result != ConnectivityResult.none;
+      if (!_isOnline) {
+        Fluttertoast.showToast(
+            msg: "Kindly check your internet connection!",
+            toastLength: Toast.LENGTH_SHORT,
+            gravity: ToastGravity.BOTTOM,
+            timeInSecForIosWeb: 1,
+            backgroundColor: Colors.red,
+            textColor: Colors.white,
+            fontSize: 16.0
+        );
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter WebView',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: _isOnline ? const SplashScreen() : const NoConnectionScreen(),
+    );
+  }
+}
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Timer(
+      const Duration(seconds: 3),
+          () => Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (context) => const WebViewScreen(url: 'https://spaceyloncanada.ca/'))), // Replace with your actual URL
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.white,
+      child: Center(
+        child: Image.asset('assets/images/splash_image.png'), // Replace with your splash image asset path
+      ),
+    );
+  }
+}
+
+class WebViewScreen extends StatefulWidget {
+  final String url;
+
+  const WebViewScreen({super.key, required this.url});
+
+  @override
+  WebViewScreenState createState() => WebViewScreenState();
+}
+
+class WebViewScreenState extends State<WebViewScreen> {
+  late WebViewController _controller;
+  bool _isLoading = true;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('SpaCeylon'),
+      ),
+      body: Stack(
+        children: [
+          WebView(
+            initialUrl: widget.url,
+            javascriptMode: JavascriptMode.unrestricted,
+            onWebViewCreated: (WebViewController webViewController) {
+              _controller = webViewController;
+            },
+            onPageStarted: (String url) {
+              setState(() {
+                _isLoading = true;
+              });
+            },
+            onPageFinished: (String url) {
+              setState(() {
+                _isLoading = false;
+              });
+            },
+            onWebResourceError: (WebResourceError error) {
+              ScaffoldMessenger.of(context).showSnackBar(
+                SnackBar(content: Text('Error loading page: ${error.description}')),
+              );
+            },
+            navigationDelegate: (NavigationRequest request) {
+              if (_isDownloadLink(request.url)) {
+                _launchInBrowser(request.url);
+                return NavigationDecision.prevent;
+              }
+              return NavigationDecision.navigate;
+            },
+          ),
+          if (_isLoading) const Center(child: CircularProgressIndicator()),
+        ],
+      ),
+    );
+  }
+  bool _isDownloadLink(String url) {
+    // Define logic to determine if the URL is a download link
+    return url.endsWith(".pdf") || url.endsWith(".zip"); // Example logic
+  }
+
+  Future<void> _launchInBrowser(String url) async {
+    final Uri uri = Uri.parse(url);
+    if (!await launchUrl(uri, mode: LaunchMode.externalApplication)) {
+      // If the URL can't be launched, you might want to inform the user
+      // You could use a Toast, Snack bar, or a dialog to notify them
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Could not launch $url')),
+      );
+    }
+  }
+}
+
+class NoConnectionScreen extends StatelessWidget {
+  const NoConnectionScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Image.asset('assets/images/no_connection.png'), // Make sure to add an image in your assets
+            const SizedBox(height: 20),
+            const Text("Kindly check your internet connection!"),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
